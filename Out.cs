11eee38a6f890011c51392d0897076e2b66b18f4
jinipk <jinipk>
2020-11-30 02:56:59 +0900

    버전 업데이트로 수정한 코드

---
+++ Out.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Out : MonoBehaviour {
+	
+	public static bool goal;
+
+	void Start() {
+		goal = false;
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+			goal = true;
+		}
+	}
+}
